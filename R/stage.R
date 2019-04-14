9cb2f2f3669bd008904952686dfd3e9f8600b814
pat-s <patrick.schratz@gmail.com>
2019-04-14 23:46:17 +0200

    move steps and name members from private to public

--- R/stage.R
+++ R/stage.R
@@ -3,13 +3,16 @@ Stage <- R6Class(
 
   public = list(
     initialize = function(name) {
-      private$name <- name
-      private$steps <- list()
+      name <- NULL
+      steps <- list()
     },
+
+    steps = list(),
+    name = NULL,
 
     add_step = function(step, code) {
       self$add_task(run = step$run, check = step$check, prepare = step$prepare,
-                    name = code)
+        name = code)
     },
 
     add_task = function(run, check = NULL, prepare = NULL, name = NULL) {
@@ -19,32 +22,30 @@ Stage <- R6Class(
         prepare = prepare %||% function() {},
         name = name %||% "<unknown task>"
       )
-      private$steps <- c(private$steps, list(step))
+      self$steps <- c(self$steps, list(step))
       invisible(self)
     },
 
     reset = function() {
-      private$steps <- list()
+      steps <- list()
     },
 
     prepare_all = function() {
-      lapply(private$steps, private$prepare_one)
+      lapply(steps, private$prepare_one)
       invisible()
     },
 
     run_all = function() {
       success <- TRUE
-      for (step in private$steps) {
+      for (step in self$steps) {
         if (!private$run_one(step)) {
-          stopc('A step failed in stage "', private$name, '": ', private$name, '.')
+          stopc('A step failed in stage "', name, '": ', name, '.')
         }
       }
     }
   ),
 
   private = list(
-    name = NULL,
-    steps = NULL,
 
     prepare_one = function(step) {
       if (identical(body(step$prepare), body(TicStep$public_methods$prepare)))
@@ -69,14 +70,117 @@ Stage <- R6Class(
     run_one = function(step) {
       if (!isTRUE(step$check())) {
         ci_cat_with_color(
-          crayon::magenta(paste0("Skipping ", private$name, ": ", step$name))
+          crayon::magenta(paste0("Skipping ", self$name, ": ", step$name))
         )
         print(step$check)
         return(TRUE)
       }
 
       ci_cat_with_color(
-        crayon::magenta(paste0("Running ", private$name, ": ", step$name))
+        crayon::magenta(paste0("Running ", self$name, ": ", step$name))
+      )
+
+      tryCatch(
+        {
+          withCallingHandlers(
+            {
+              step$run()
+              TRUE
+            },
+            error = function(e) {
+              ci_cat_with_color(crayon::red(paste0("Error: ", conditionMessage(e))))
+              tb <- format_traceback()
+              ci_cat_with_color(crayon::yellow(tb))
+            }
+          )
+        },
+        error = function(e) {
+          FALSE
+        }
+      )
+    }
+  )
+)
+Stage <- R6Class(
+  "Stage",
+
+  public = list(
+    initialize = function(name) {
+      name <- NULL
+      steps <- list()
+    },
+
+    steps = list(),
+    name = NULL,
+
+    add_step = function(step, code) {
+      self$add_task(run = step$run, check = step$check, prepare = step$prepare,
+        name = code)
+    },
+
+    add_task = function(run, check = NULL, prepare = NULL, name = NULL) {
+      step <- list(
+        run = run,
+        check = check %||% function() TRUE,
+        prepare = prepare %||% function() {},
+        name = name %||% "<unknown task>"
+      )
+      self$steps <- c(self$steps, list(step))
+      invisible(self)
+    },
+
+    reset = function() {
+      steps <- list()
+    },
+
+    prepare_all = function() {
+      lapply(steps, private$prepare_one)
+      invisible()
+    },
+
+    run_all = function() {
+      success <- TRUE
+      for (step in self$steps) {
+        if (!private$run_one(step)) {
+          stopc('A step failed in stage "', name, '": ', name, '.')
+        }
+      }
+    }
+  ),
+
+  private = list(
+
+    prepare_one = function(step) {
+      if (identical(body(step$prepare), body(TicStep$public_methods$prepare)))
+        return()
+
+      if (!isTRUE(step$check())) {
+        ci_cat_with_color(
+          crayon::magenta(paste0("Skipping prepare: ", step$name))
+        )
+        print(step$check)
+        return()
+      }
+
+      ci_cat_with_color(
+        crayon::magenta(paste0("Preparing: ", step$name))
+      )
+      step$prepare()
+
+      invisible()
+    },
+
+    run_one = function(step) {
+      if (!isTRUE(step$check())) {
+        ci_cat_with_color(
+          crayon::magenta(paste0("Skipping ", self$name, ": ", step$name))
+        )
+        print(step$check)
+        return(TRUE)
+      }
+
+      ci_cat_with_color(
+        crayon::magenta(paste0("Running ", self$name, ": ", step$name))
       )
 
       tryCatch(
