4b6fec98c32f0290bd240714e91e91a84a25c356
pat-s <patrick.schratz@gmail.com>
2019-05-09 22:50:26 +0200

    test styler deploy

--- tic.R
+++ tic.R
@@ -6,3 +6,7 @@ do_package_checks(error_on = if (getRversion() >= "3.2") "warning" else "error")
 if (ci_has_env("BUILD_PKGDOWN")) {
   do_pkgdown()
 }
+
+get_stage("deploy") %>%
+  add_code_step(styler::style_pkg()) %>%
+  add_step(step_push_deploy())
