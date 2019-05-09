a87b7d6e77c19767c00d941b4d97ac2874bb088c
pat-s <patrick.schratz@gmail.com>
2019-05-10 00:37:14 +0200

    new approach

--- R/steps-styler.R
+++ R/steps-styler.R
@@ -1,17 +1,31 @@
-#' @include steps-rcmdcheck.R
+#' @include steps-styler.R
 StylePkg = R6Class(
   "StylePkg",
   inherit = TicStep,
 
   public = list(
-    initialize = function(...) {
-      private$styler_args = list(...)
-      super$initialize()
+    initialize = function(pkg = ".", ..., style = styler::tidyverse_style,
+      transformers = style(...), filetype = "R",
+      exclude_files = "R/RcppExports.R", include_roxygen_examples = TRUE) {
+
+      private$style = style
+      private$transformers = transformers
+      private$filetype = filetype
+      private$exclude_files = exclude_files
+      private$include_roxygen_examples = include_roxygen_examples
+
+      private$super$initialize()
     },
 
     run = function() {
       remotes::install_local(".")
-      do.call(styler::style_pkg, c(list(preview = FALSE), private$styler_args))
+      styler::style_pkg(
+        style = private$style,
+        transformers = private$transformers,
+        filetype = filetype,
+        exclude_files = exclude_files,
+        include_roxygen_examples = include_roxygen_examples
+        )
     },
 
     prepare = function() {
@@ -20,17 +34,46 @@ StylePkg = R6Class(
     }),
 
   private = list(
-    styler_args = NULL
+    ... = NULL,
+    style = NULL,
+    transformers = NULL,
+    filetype = NULL,
+    exclude_files = NULL,
+    include_roxygen_examples = NULL
   )
 )
 
-#' Step: Style package using the [styler] package
-#'
+#' Step: Style package using the `styler` package
 #' Styles a R package using `styler::style_pkg()`
 #'
-#' @inheritDotParams styler::style_pkg
+#' @param ... Ignored, used to enforce naming of arguments.
+#' @param pkg Path to package.
+#' @param style A function that creates a style guide to use, by default
+#'   `tidyverse_style()` (without the parentheses). Not used
+#'   further except to construct the argument `transformers`. See
+#'   `style_guides()` for details.
+#' @param transformers A set of transformer functions. This argument is most
+#'   conveniently constructed via the `style` argument and `...`. See
+#'   'Examples'.
+#' @param filetype Vector of file extensions indicating which file types should
+#'   be styled. Case is ignored, and the `.` is optional, e.g. `c(".R", ".Rmd",
+#'   ".Rnw")` or `c("r", "rmd", "rnw")`.
+#' @param exclude_files Character vector with paths to files that should be
+#'   excluded from styling.
+#' @param include_roxygen_examples Whether or not to style code in roxygen
+#'   examples.
 #' @family steps
 #' @export
-step_style_pkg = function(...) {
-  StylePkg$new(...)
+step_style_pkg = function(...,
+  pkg = ".", style = styler::tidyverse_style,
+  transformers = style(...), filetype = "R",
+  exclude_files = "R/RcppExports.R", include_roxygen_examples = TRUE) {
+
+  StylePkg$new(
+    style = style,
+    transformers = transformers,
+    filetype = filetype,
+    exclude_files = exclude_files,
+    include_roxygen_examples = include_roxygen_examples
+  )
 }
