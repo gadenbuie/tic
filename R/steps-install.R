7828693c5905bc41c50c2604c3637552924ea3be
pat-s <patrick.schratz@gmail.com>
2019-09-23 22:34:16 +0200

    try renv

--- R/steps-install.R
+++ R/steps-install.R
@@ -21,7 +21,9 @@ InstallDeps <- R6Class(
           dependencies = TRUE, repos = private$repos, build = FALSE
         )
       )
-    }
+      renv::init()
+      renv::install()
+      }
   ),
 
   private = list(
